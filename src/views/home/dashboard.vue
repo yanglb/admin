d83f36492f9acbb57025018f31cef1002fb31ca1
yanglb <me@yanglb.com>
2021-01-02 19:09:07 +0800

    清理无用代码中

--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -1,28 +1,5 @@
 <template>
   <div class="container-fluid">
-    <!-- Small boxes (Stat box) -->
-    <wg-dev-count />
-
-    <!-- /.row -->
-    <!-- Main row -->
-    <div class="row">
-      <!-- Left col -->
-      <section class="col-lg-7 connectedSortable">
-        <wg-today />
-        <wg-daily />
-      </section>
-      <!-- /.Left col -->
-      <!-- right col (We are only adding the ID to make the widgets sortable)-->
-      <section class="col-lg-5 connectedSortable">
-        <!-- Map card -->
-        <wg-vector-map />
-
-        <!-- TO DO List -->
-        <wg-todo-list />
-        <!-- /.card -->
-      </section>
-      <!-- right col -->
-    </div>
-    <!-- /.row (main row) -->
+    <p>暂无内容</p>
   </div><!-- /.container-fluid -->
 </template>
