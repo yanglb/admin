36c7aec3a8010612305296f979c4302084ddc732
杨利兵 <me@yanglb.com>
2021-01-12 13:18:07 +0800

    添加模拟数据

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,8 @@
-import { get, post, del } from 'axios'
+import { post, del } from 'axios'
 
 const base = '/auth'
 
-export const signin = (username, password) => post(base, { username, password })
-export const info = () => get(base)
+export const signin1 = (username, password) => post(base, { username, password })
+export const signin = () => Promise.resolve({ "user": { "id": 1, "username": "admin", "name": "管理员" }, "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsIm5hbWUiOiLnrqHnkIblkZgiLCJpYXQiOjE2MTA0Mjg0ODV9.ZBZrmkKJThMF0bCCb0aIpNXSNYoKbrRwKaLh8XiEjVM", "type": "Bearer" })
+export const info = () => Promise.resolve({ "id": 1, "username": "admin", "name": "管理员", "iat": 1610428485 })
 export const signout = () => del(base)
--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="container-fluid">
-    <p>暂无内容</p>
+    <!-- Small boxes (Stat box) -->
+    <wg-dev-count />
+
+    <!-- /.row -->
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <section class="col-lg-7 connectedSortable">
+        <wg-today />
+        <wg-daily />
+      </section>
+      <!-- /.Left col -->
+      <!-- right col (We are only adding the ID to make the widgets sortable)-->
+      <section class="col-lg-5 connectedSortable">
+        <!-- Map card -->
+        <wg-vector-map />
+
+        <!-- TO DO List -->
+        <wg-todo-list />
+        <!-- /.card -->
+      </section>
+      <!-- right col -->
+    </div>
+    <!-- /.row (main row) -->
   </div><!-- /.container-fluid -->
 </template>
